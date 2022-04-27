09c1a65dd368e3900a0d9f849a66d9a46356e4e1
tian <2426793192@qq.com>
2022-04-27 16:00:12 +0800

    hot fix

--- src/main/java/com/tian/test/Test.java
+++ src/main/java/com/tian/test/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello !");
         System.out.println("hello 2!");
+        System.out.println("hello hot-fix!");
     }
 }
